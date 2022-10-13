f7752763bf663d88726a49aef8d9ca5529996fda
Amit Nir <amitnir95@gmail.com>
2022-10-13 16:41:49 +0300

    Added a search bar filter * When more than 3 characters are typed, every change will filter the items

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,40 @@
 import React from "react";
+import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const SearchBox = (props) => {
+  const changeHandler = (ev) => {
+    ev.preventDefault();
+    if (ev.target.value.length >= 3) {
+      props.onSearchChanged(
+        ev.target.value,
+        (page) => agent.Items.byTitle(ev.target.value, page),
+        agent.Items.byTitle(ev.target.value)
+      );
+    }
+    else {
+      props.onSearchChanged(
+        '',
+        (page) => agent.Items.byTitle('', page),
+        agent.Items.byTitle('')
+      );
+
+    }
+  };
+  return (
+      <input id="search-box" placeholder="what is it that you trully desire?" onChange={changeHandler}></input>
+    );
+};
+
+
+const Banner = (props) => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <SearchBox onSearchChanged={props.onSearchChanged}/>
           <span> the cool stuff.</span>
         </div>
       </div>
